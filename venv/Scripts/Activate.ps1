6e363c67da9bd60998e767817e5348a99a3ec7e8
atharva sharma <70029832+AtharvaSharma7@users.noreply.github.com>
2021-10-01 00:43:04 +0530

    Update Activate.ps1

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -1,12 +1,9 @@
 function global:deactivate ([switch]$NonDestructive) {
-    # Revert to original values
-    if (Test-Path function:_OLD_VIRTUAL_PROMPT) {
-        copy-item function:_OLD_VIRTUAL_PROMPT function:prompt
-        remove-item function:_OLD_VIRTUAL_PROMPT
+    
     }
 
     if (Test-Path env:_OLD_VIRTUAL_PYTHONHOME) {
-        copy-item env:_OLD_VIRTUAL_PYTHONHOME env:PYTHONHOME
+        copy-item env:_OLD_VIRTUAL_PYTHON;jbiHOME env:PYTHONHOME
         remove-item env:_OLD_VIRTUAL_PYTHONHOME
     }
 
@@ -15,7 +12,7 @@ function global:deactivate ([switch]$NonDestructive) {
         remove-item env:_OLD_VIRTUAL_PATH
     }
 
-    if (Test-Path env:VIRTUAL_ENV) {
+    else (Test-Path env:VIRTUAL_ENV) {
         remove-item env:VIRTUAL_ENV
     }
 
