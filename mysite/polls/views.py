c504c5d021c0d76c16d45e93606fe547211fbda3
AnnaSrivastava <ananyavsrivastava@gmail.com>
2021-10-01 21:00:45 +0530

    added retract functionality

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -38,3 +38,15 @@ def vote(request,question_id):
         selected_choice.save()
 
         return HttpResponseRedirect(reverse('polls:results',args=(question_id,)))
+def retract(request,question_id):
+    #return HttpResponse("Votes on question :%s"%question_id)
+    question=get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice=question.choice_set.get(pk=request.POST['choice'])
+    except:
+        return render(request,'polls/details.html',{'question':question,'error_message':"plz select a choice"})
+    else:
+        selected_choice.votes-=1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results',args=(question_id,)))
